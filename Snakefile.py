6947709ae5e2b6eaa2cacec3050a8c3287f5cef0
nukandtudasmom <31939830+nukandtudasmom@users.noreply.github.com>
2022-10-10 13:27:09 -0500

    Update Snakefile.py

--- Snakefile.py
+++ Snakefile.py
@@ -1,7 +1,10 @@
+import re
+## importing REGEX library
 targets =[
-    'line 8', 
+    'GNS3', 
     'team',
-    'search'
+    'search',
+    'issues'
     ]
 with open(r'myfile.txt', 'r') as file:
     # read all content from a file using the "read" function
@@ -9,20 +12,9 @@ with open(r'myfile.txt', 'r') as file:
     # check if keywords exist
     for target in targets:
         if target in content: 
-            print ('string exists')
+            print ('string exists' ' + ' (targets))
+            
         else:
             print('string does not exist')
-# the string we are searching for
-# todo - print results of findings, adding additional search criteria to array, switch from strings to regex
-
-
-
-# another try at searching for a string in a document.
-# with open (r'myfile.txt', 'r') as fp:
-# lines = fp.readlines()
-# for row in lines:
-# word = 'line 3'
- #   print(row.find(word))
-  #  if row.find(word) == 0:
-  #  print('string exists in file')
- #       print ('line number:', lines.index(line))
+            
+    #file.write(content)
