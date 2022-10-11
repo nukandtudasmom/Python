139783256608584fd920aa9e95c3ab2f9255620b
nukandtudasmom <31939830+nukandtudasmom@users.noreply.github.com>
2022-10-11 12:53:01 -0500

    Update Snakefile.py

--- Snakefile.py
+++ Snakefile.py
@@ -1,20 +1,36 @@
 import re
-## importing REGEX library
 targets =[
-    'GNS3', 
+    'line 8', 
     'team',
-    'search',
-    'issues'
+    'search'
     ]
 with open(r'myfile.txt', 'r') as file:
     # read all content from a file using the "read" function
     content = file.read()
-    # check if keywords exist
-    for target in targets:
-        if target in content: 
-            print ('string exists' ' + ' (targets))
-            
-        else:
-            print('string does not exist')
-            
-    #file.write(content)
+    #print(content)
+
+   # check if keywords exist
+   # x = re.findall ("GNS3")
+    # x = bool(re.search(r'^l', targets[0]))
+    # if x:
+    #         print(f"yes, {targets[0]} exists!")
+    x = re.findall(r'^l', targets[])
+    print(x)
+    y = re.findall(r'$ch', targets[2])
+    print(y)
+            #print(f"yes, {targets[2]} exists!")
+
+# the string we are searching for
+# todo - print results of findings, adding additional search criteria to array, switch from strings to regex
+
+
+
+# another try at searching for a string in a document.
+# with open (r'myfile.txt', 'r') as fp:
+# lines = fp.readlines()
+# for row in lines:
+# word = 'line 3'
+ #   print(row.find(word))
+  #  if row.find(word) == 0:
+  #  print('string exists in file')
+ #       print ('line number:', lines.index(line))
